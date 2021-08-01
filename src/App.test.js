dfbdd22afc2a7f9ca8f3abe9203cfc4c13800247
Michael Draheim <mdraheim@draheim.io>
2021-08-01 10:55:51 -0700

    initial commit.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('word friend in text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wordFriend = screen.getByText(/friend/i);
+  expect(wordFriend).toBeInTheDocument();
+});
+
+test('word friend in text', () => {
+  render(<App />);
+  const wordFriend = screen.getByText(/friend/i);
+  expect(wordFriend).toBeInTheDocument();
+});
+
+test('word friend in text', () => {
+  render(<App />);
+  const wordFriend = screen.getByText(/friend/i);
+  expect(wordFriend).toBeInTheDocument();
 });
